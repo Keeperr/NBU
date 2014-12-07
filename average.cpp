1f5a8306889193e0167d13c4d66a93f67e6df000
Keeperr <keeperr7@gmail.com>
2014-12-07 13:07:05 +0200

    Create average.cpp

---
+++ average.cpp
@@ -0,0 +1,25 @@
+/*
+    FacultyN: F74665
+    Author: Ivan Petrov
+    HW-set: 2
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+
+    double a, b;
+
+    cout << "Enter number1: ";
+    cin >> a;
+
+    cout << "Enter number2: ";
+    cin >> b;
+
+    cout << (a+b)/2;
+
+    return 0;
+}
